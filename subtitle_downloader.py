231ffd7457804ea5b1a231b3b2e98e54186619d1
iman <maityimankalyan@gmail.com>
2019-09-22 00:11:33 +0530

    defined download_subtitles() and called if calling from this file only
    checking if __name__ == "__main__"

--- subtitle_downloader.py
+++ subtitle_downloader.py
@@ -14,26 +14,39 @@ VIDEO_EXTENSIONS = ('.3g2', '.3gp', '.3gp2', '.3gpp', '.60d', '.ajp', '.asf', '.
                     '.vob', '.vro', '.wm', '.wmv', '.wmx', '.wrap', '.wvx', '.wx', '.x264', '.xvid')
 
 SUBTITLE_EXT = ('.sub', '.smi', '.txt', '.ssa', '.ass', '.mpl')
-parser = ArgumentParser()
-parser.add_argument("-d", "--directory", dest="directory",
-                    help='example: -d <directory path>. default is current directory', type=str, default=os.getcwd())
-parser.add_argument("-l", "--language", dest="language",
-                    help="example: -l <en|fr>. default is English", type=str, default='en')
-args = parser.parse_args()
 
-print("[User] checking files in - {}".format(args.directory))
-all_files = [os.path.join(args.directory, each_file) for each_file in os.listdir(args.directory)]
-all_video_files = []
-for each_ext in VIDEO_EXTENSIONS:
-    temp_video_files = [each_file for each_file in all_files if each_file.endswith(each_ext)]
-    all_video_files += temp_video_files
-print("[User] all video files are: {}".format('\n'.join(all_video_files)))
-for each_video in all_video_files:
-    cmd = 'subliminal download -l {} "{}"'.format(args.language, each_video)
-    print("[User] Downloading subtitles for: {}".format(each_video))
-    p = subprocess.Popen(cmd, shell=True)
-for file in os.listdir( ):
-    for ext in SUBTITLE_EXT:
-        lan_ext = '.{}{}'.format(args.language, ext)
-        if file.endswith(lan_ext):
-            os.rename(file , file.replace(lan_ext , ext))
+
+def download_subtitles(args):
+    """Function to process supported video files and download subtitles for them
+    Parameters:
+        args (ArgumentParser object): contains all the command line args
+    Returns:
+        1 (int): for success
+    """
+    print("[User] checking files in - {}".format(args.directory))
+    all_files = [os.path.join(args.directory, each_file) for each_file in os.listdir(args.directory)]
+    all_video_files = []
+    for each_ext in VIDEO_EXTENSIONS:
+        temp_video_files = [each_file for each_file in all_files if each_file.endswith(each_ext)]
+        all_video_files += temp_video_files
+    print("[User] all video files are: {}".format('\n'.join(all_video_files)))
+    for each_video in all_video_files:
+        cmd = 'subliminal download -l {} "{}"'.format(args.language, each_video)
+        print("[User] Downloading subtitles for: {}".format(each_video))
+        p = subprocess.Popen(cmd, shell=True)
+    for file in os.listdir( ):
+        for ext in SUBTITLE_EXT:
+            lan_ext = '.{}{}'.format(args.language, ext)
+            if file.endswith(lan_ext):
+                os.rename(file , file.replace(lan_ext , ext))
+    return 1
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-d", "--directory", dest="directory",
+                        help='example: -d <directory path>. default is current directory', type=str, default=os.getcwd())
+    parser.add_argument("-l", "--language", dest="language",
+                        help="example: -l <en|fr>. default is English", type=str, default='en')
+    args = parser.parse_args()
+    download_subtitles(args)
